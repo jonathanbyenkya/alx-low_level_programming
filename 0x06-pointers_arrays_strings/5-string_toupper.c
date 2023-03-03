b618392610d84f0c48d89494abd724d28fb7c9a5
jonathanbyenkya <jonibyenkya@gmail.com>
2023-03-03 00:09:27 +0000

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,14 +9,14 @@
  */
 char *string_toupper(char *str)
 {
-	int index = 0;
+	int i = 0;
 
-	while (str[index])
+	while (str[i])
 	{
-		if (str[index] >= 'a' && str[index] <= 'z')
-			str[index] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= 32;
 
-		index++;
+		i++;
 	}
 
 	return (str);
