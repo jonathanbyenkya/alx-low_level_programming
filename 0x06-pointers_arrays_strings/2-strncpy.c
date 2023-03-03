2397817e6550981feecdd80f2a172f9777bfd669
jonathanbyenkya <jonibyenkya@gmail.com>
2023-03-03 00:18:57 +0000

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,16 +10,16 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int index = 0, src_len = 0;
+	int i = 0, src_len = 0;
 
-	while (src[index++])
+	while (src[i++])
 		src_len++;
 
-	for (index = 0; src[index] && index < n; index++)
-		dest[index] = src[index];
+	for (i = 0; src[i] && i < n; i++)
+		dest[i] = src[i];
 
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';
+	for (i = src_len; i < n; i++)
+		dest[i] = '\0';
 
 	return (dest);
 }
